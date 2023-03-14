348ac32ed7f76328cdd275650569d1f6f04ec0ea
Mohammad-Sufyan-Azam <sufyan20312@iiitd.ac.in>
2023-03-14 23:07:15 +0530

    Added largest_unique_substr and palin_substr code

--- 2 Arrays and Strings/Largest Unique Substring.cpp
+++ 2 Arrays and Strings/Largest Unique Substring.cpp
@@ -2,4 +2,43 @@
 Given a string S, find the largest substring with no repetition i.e. largest substring which contain all unique characters.
 */
 
+// input - given string
+#include <bits/stdc++.h>
+using namespace std;
+// output - output string that contains largest unique substring
+
+void findLargestUniqueSubstring(char input[], char output[]) {
+	// Write your code here
+	int n = strlen(input);
+	int s = 0, e = 0, p1 = 0, p2 = 0;
+	unordered_map<char, int> hash;
+	int ans = -1;
+	for(e=0; e<n; e++) {
+		if(hash.find(input[e]) == hash.end()) {
+			hash[input[e]] = 1;
+			if(e-s+1>ans) {
+				ans =max(ans, e-s+1);
+				p2=e;
+				p1=s;
+			}
+		}
+		else {
+			while(input[s] != input[e]) {
+				hash.erase(input[s]);
+				s++;
+			}
+			s++;
+		}
+	}
+	// cout<<ans<<"\n";
+	int ptr = 0;
+	for(int i = p1; i <= p2; i++) {
+		output[ptr++] = input[i];
+		// cout<<output[i];
+	}
+	// cout<<p1<<" "<<p2<<"\n";
+	output[ptr] = '\0';
+	
+	// cout<<"\n";
+}
 
--- 2 Arrays and Strings/Palindrome substrings.cpp
+++ 2 Arrays and Strings/Palindrome substrings.cpp
@@ -2,4 +2,40 @@
 Given a string S, count and return the number of substrings of S that are palindrome.
 Single length substrings are also palindromes. You just need to calculate the count, not the substrings.
 */
+#include <string.h>
+using namespace std;
+
+bool ispalin(string s) {
+	for(int i=0; i<s.size()/2; i++) {
+		if (s[i] != s[s.size()-i-1])
+			return false;
+	}
+	return true;
+}
+
+int countPalindromeSubstrings(char arr[]) {
+	// Write your code here
+	int n = 0, count = 0;
+	for(; arr[n] != '\0'; n++);
+
+	// O(n^2), O(n)
+	for(int i=0; i<n; i++) {
+
+		int s = i, e = i;
+		while( (s>=0 && e < n) && arr[s] == arr[e] ) {
+			s--;
+			e++;
+			count++;
+		}
+
+		s = i, e = i+1;
+		while( (s>=0 && e < n) && arr[s] == arr[e] ) {
+			s--;
+			e++;
+			count++;
+		}
+	}
+
+	return count;
+}
 
